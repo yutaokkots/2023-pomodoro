688ff466126b5c9be5354a08caa714cb4987957a
yutaokkots <yuta.okkotsu@gmail.com>
2023-05-22 15:06:29 -0600

    consolidated start and stop buttons

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import TimerCountDownDisplay from './Components/TimerCountDownDisplay';
+import { TimerCountDownDisplay } from './Components/TimerCountDownDisplay';
 
 
 const FOCUS_TIME_MIN = 0.2 * 60 * 1000;   // 12 seconds
@@ -15,27 +15,30 @@ const REST_TIME_MIN = 0.1 * 60 * 1000;    // 6 seconds
 export default function App() {
     const [timerCount, setTimerCount] = useState<number>(FOCUS_TIME_MIN);
     const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | null>(null)
+    const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false)
+
 
     const startTimer = () => {
-        const id = setInterval(() => setTimerCount(prev => prev - 1000), 1000)
+        const id = setInterval(() => setTimerCount((prev) => prev - 1000), 1000)
         setTimerInterval(id)
+        setIsTimerRunning(true)
       }
 
     const pauseTimer = () => {
         if (timerInterval !== null){
             clearInterval(timerInterval)
         }
+        setIsTimerRunning(false)
     }
 
-    const timerDate = new Date(timerCount)
+    
 
     return (
         <View style={styles.container}>
             <Text>Open up App.tsx to start working on your app!</Text>
             <StatusBar style="auto" />
-            <Button title="Start" onPress={startTimer}/>
-            <Button title="Stop" onPress={pauseTimer}/>
-            <TimerCountDownDisplay timerDate={ timerDate } />
+            <Button title={isTimerRunning ? 'Stop': 'Start'} onPress={isTimerRunning ? pauseTimer: startTimer}/>
+            <TimerCountDownDisplay timerDate={ new Date(timerCount) } />
         </View>
     );
 }
